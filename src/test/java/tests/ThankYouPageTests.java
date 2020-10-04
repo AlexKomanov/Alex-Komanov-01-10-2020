c92f4ed9d586acb49c9d2c4ab5345e328be3c76b
Alex Komanov <akomanov@seatgeek.com>
2020-10-04 07:08:03 +0300

    AUT-5 Refactoring ThankYouPage tests

--- src/test/java/tests/ThankYouPageTests.java
+++ src/test/java/tests/ThankYouPageTests.java
@@ -1,10 +1,14 @@
 package tests;
 
+import io.qameta.allure.*;
 import org.testng.annotations.Test;
 
 /**
  * @author Alex Komanov
  */
+
+@Epic("Thank You Page")
+@Feature("Thank You Page General Tests")
 public class ThankYouPageTests extends BaseTest{
 
     private String name = "Alex";
@@ -16,20 +20,32 @@ public class ThankYouPageTests extends BaseTest{
     String pageUrl = "https://automation.herolo.co.il/thank-you/";
 
 
-    @Test
+    @Link(value = "AUT-TP-1", name = "#")
+    @Story("Basic Tests")
+    @Description("Test validates correct redirection to 'Thank You' page via footer form")
+    @Severity(SeverityLevel.CRITICAL)
+    @Test(description = "01_Redirection via footer form")
     public void test_01(){
         steps.fillCorrectFooterForm(name, emailCorrect, phoneCorrect)
                 .validateCorrectRedirection(mainTitle, pageTitle, pageUrl)
                 .backToMainPage();
     }
 
-    @Test
+    @Link(value = "AUT-TP-2", name = "#")
+    @Story("Basic Tests")
+    @Description("Test validates correct redirection to 'Thank You' page via lower form")
+    @Severity(SeverityLevel.CRITICAL)
+    @Test(description = "02_Redirection via lower form")
     public void test_02(){
         steps.fillCorrectLowerForm(name, company, emailCorrect, phoneCorrect)
                 .validateCorrectRedirection(mainTitle, pageTitle, pageUrl);
     }
 
-    @Test
+    @Link(value = "AUT-TP-3", name = "#")
+    @Story("Basic Tests")
+    @Description("Test validates correct redirection to social media pages")
+    @Severity(SeverityLevel.CRITICAL)
+    @Test(description = "03_Redirection to social media pages")
     public void test_03(){
         thankYouPageSteps
                 .navigateToSocialMedia("LinkedIn")
