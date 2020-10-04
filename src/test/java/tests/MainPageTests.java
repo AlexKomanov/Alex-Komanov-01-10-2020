ae714f536807c4272c05a1ce0bf5562cbc4df683
Alex Komanov <akomanov@seatgeek.com>
2020-10-04 07:31:25 +0300

    AUT-4 Refactoring MainPage tests

--- src/test/java/tests/MainPageTests.java
+++ src/test/java/tests/MainPageTests.java
@@ -1,7 +1,6 @@
 package tests;
 
-import io.qameta.allure.Epic;
-import io.qameta.allure.Feature;
+import io.qameta.allure.*;
 import org.testng.annotations.Test;
 
 /**
@@ -9,7 +8,7 @@ import org.testng.annotations.Test;
  */
 
 @Epic("Main Page")
-@Feature("Base Test Feature")
+@Feature("Main Page General Tests")
 public class MainPageTests extends BaseTest {
 
     private String blankField = "";
@@ -21,13 +20,21 @@ public class MainPageTests extends BaseTest {
     private String emailIncorrect = "gmail.com";
 
 
-    @Test
+    @Link(value = "AUT-MP-1", name = "#")
+    @Story("Basic Tests")
+    @Description("Test validates action of 'Back to top' button")
+    @Severity(SeverityLevel.CRITICAL)
+    @Test(description = "01_Action of 'Back to top' button")
     public void test_01() {
         steps.scrollDown()
                 .validateBackToTopButtonAction();
     }
 
-    @Test
+    @Link(value = "AUT-MP-2", name = "#")
+    @Story("Basic Tests")
+    @Description("Test validates action of 'Next' button")
+    @Severity(SeverityLevel.CRITICAL)
+    @Test(description = "02_Action of 'Next' button")
     public void test_02() {
         steps.clickOnSlickArrow("Next")
                 .validateCorrectSwitchOfArrow("Next")
@@ -35,7 +42,11 @@ public class MainPageTests extends BaseTest {
                 .validateCorrectSwitchOfArrow("Next");
     }
 
-    @Test
+    @Link(value = "AUT-MP-3", name = "#")
+    @Story("Basic Tests")
+    @Description("Test validates action of 'Previous' button")
+    @Severity(SeverityLevel.CRITICAL)
+    @Test(description = "03_Action of 'Previous' button")
     public void test_03() {
         steps.clickOnSlickArrow("Previous")
                 .validateCorrectSwitchOfArrow("Previous")
@@ -43,7 +54,11 @@ public class MainPageTests extends BaseTest {
                 .validateCorrectSwitchOfArrow("Previous");
     }
 
-    @Test
+    @Link(value = "AUT-MP-4", name = "#")
+    @Story("Basic Tests")
+    @Description("Test validates correct behaviour of footer form")
+    @Severity(SeverityLevel.CRITICAL)
+    @Test(description = "04_Correct behaviour of footer form")
     public void test_04() {
         steps.validateFooterForm("Displayed")
                 .scrollDown()
@@ -52,7 +67,11 @@ public class MainPageTests extends BaseTest {
                 .validateFooterForm("Displayed");
     }
 
-    @Test
+    @Link(value = "AUT-MP-5", name = "#")
+    @Story("Basic Tests")
+    @Description("Test validates correct error messages of lower form")
+    @Severity(SeverityLevel.CRITICAL)
+    @Test(description = "05_Correct error messages of lower form")
     public void test_05() {
         steps.scrollDown()
                 .fillIncorrectLowerForm(blankField, blankField, blankField, blankField)
@@ -65,7 +84,11 @@ public class MainPageTests extends BaseTest {
                 .validateFieldError("Phone", "מספר טלפון לא חוקי");
     }
 
-    @Test
+    @Link(value = "AUT-MP-6", name = "#")
+    @Story("Basic Tests")
+    @Description("Test validates correct error messages of footer form")
+    @Severity(SeverityLevel.CRITICAL)
+    @Test(description = "06_Correct error messages of footer form")
     public void test_06() {
         steps.backToTop()
                 .fillIncorrectFooterForm(blankField, blankField, blankField)
@@ -77,35 +100,32 @@ public class MainPageTests extends BaseTest {
                 .validateFieldError("FooterPhone", "מספר טלפון לא חוקי");
     }
 
+/**
+ *  *********** Optional scenario with Allure attachments ***********
 
+    //@Step("Writing '{value}' in the E-Mail field")
+      @Link(value = "AUT-122", name = "https://seatgeekenterprise.atlassian.net/browse/AUT-122")
+      @Test(dataProvider = "dataProvider", description = "Write E-Mail")
+      @Story("I want to check error messages")
+      @Description("Fills only the E-Mail field")
+      @Severity(SeverityLevel.CRITICAL)
+      public void openMainSite(String value) {
 
-
-
-//    //@Step("Writing '{value}' in the E-Mail field")
-//    @Link(value = "AUT-122", name = "https://seatgeekenterprise.atlassian.net/browse/AUT-122")
-//    @Test(dataProvider = "dataProvider", description = "Write E-Mail")
-//    @Story("I want to check error messages")
-//    @Description("Fills only the E-Mail field")
-//    @Severity(SeverityLevel.CRITICAL)
-//    public void openMainSite(String value) {
-//
-//        steps.fillFooterNameFieldAndSubmit(value)
-//                .validateFooterEmailErrorMessage("שדה אימייל הוא שדה חובה")
-//                .validateFooterPhoneErrorMessage("שדה טלפון הוא שדה חובה");
-//        AllureAttachment.addTextAttachment("Finished...");
-//        AllureAttachment.attachCSV("USED VALUE \n" + value);
-//        AllureAttachment.captureScreenshot(BaseTest.getDriver());
-//        AllureAttachment.attachJson("{\n" +
-//                "  \"test\": \"Automation Test\",\n" +
-//                "  \"values\": {\n" +
-//                "    \"type\": \"Email\",\n" +
-//                "    \"value\": \""+value+"\"\n" +
-//                "  }\n" +
-//                "}");
-//
-//
-//
-//    }
-
-
+          steps.fillFooterNameFieldAndSubmit(value)
+                  .validateFooterEmailErrorMessage("שדה אימייל הוא שדה חובה")
+                  .validateFooterPhoneErrorMessage("שדה טלפון הוא שדה חובה");
+          AllureAttachment.addTextAttachment("Finished...");
+          AllureAttachment.attachCSV("USED VALUE \n" + value);
+          AllureAttachment.captureScreenshot(BaseTest.getDriver());
+          AllureAttachment.attachJson("{\n" +
+                  "  \"test\": \"Automation Test\",\n" +
+                  "  \"values\": {\n" +
+                  "    \"type\": \"Email\",\n" +
+                  "    \"value\": \""+value+"\"\n" +
+                  "  }\n" +
+                  "}");
+ */
 }
+
+
+
